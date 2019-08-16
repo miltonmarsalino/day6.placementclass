16aa125a5300bd10601f0c16d1ec3fd534037461
miltonmarsalino <46523397+miltonmarsalino@users.noreply.github.com>
2019-08-16 09:08:07 +0530

    Create stackpushpop.py

---
+++ stackpushpop.py
@@ -0,0 +1,13 @@
+# your code goes here
+s = stack()
+print(s.isempty())
+s.push(4)
+s.push("dog")
+print(s.peek())
+s.push(true)
+print(s.size())
+print(s.isempty())
+s.push(8.4)
+print(s.pop())
+print(s.pop())
+print(s.size())
